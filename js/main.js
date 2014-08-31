72f3d57cc4c7e7ac951ad8518021bf66774a2469
julia- <juliaryan@Julias-MacBook-Pro-3.local>
2014-09-01 00:58:30 +1000

    added google map

--- js/main.js
+++ js/main.js
@@ -35,68 +35,68 @@ $(document).ready(initialise);
 
 // --- GOOGLEMAP --- //
 
-// var myLatLng = new google.maps.LatLng(-33.863903, 151.20845);
+var myLatLng = new google.maps.LatLng(-33.863903, 151.20845);
 
-// function googleMapStyles() {
-// 	var styles = [
-//     {
-//         "stylers": [
-//             {
-//                 "hue": "#dd0d0d"
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "road",
-//         "elementType": "labels",
-//         "stylers": [
-//             {
-//                 "visibility": "on"
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "road",
-//         "elementType": "geometry",
-//         "stylers": [
-//             {
-//                 "lightness": 100
-//             },
-//             {
-//                 "visibility": "simplified"
-//             }
-//         ]
-//     }
-//     ];
-// 	var styledMap = new google.maps.StyledMapType(styles,
-// 		{name: "Styled Map"});
-// 	var mapOptions = {
-// 		center: myLatLng,
-// 		zoom: 16,
-// 		disableDefaultUI: true,
-// 		navigationControl: false,
-// 		mapTypeControl: false,
-// 		scaleControl: false,
-// 		draggable: false,
-// 		scrollwheel: false,
-// 		mapTypeControlOptions: {
-// 			mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map-style']
-// 		}
-// 	};
-// 	var marker = new google.maps.Marker({
-// 		position: myLatLng,
-// 		map: map,
-// 		icon: 'images/bat.png'
-// 	});
+function googleMapStyles() {
+	var styles = [
+    {
+        "stylers": [
+            {
+                "hue": "#dd0d0d"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "lightness": 100
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    }
+    ];
+	var styledMap = new google.maps.StyledMapType(styles,
+		{name: "Styled Map"});
+	var mapOptions = {
+		center: myLatLng,
+		zoom: 16,
+		disableDefaultUI: true,
+		navigationControl: false,
+		mapTypeControl: false,
+		scaleControl: false,
+		draggable: false,
+		scrollwheel: false,
+		mapTypeControlOptions: {
+			mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map-style']
+		}
+	};
+	var marker = new google.maps.Marker({
+		position: myLatLng,
+		map: map,
+		icon: 'images/bat.png'
+	});
 
-// 	var map = new google.maps.Map(document.getElementById('map-canvas'),
-// 		mapOptions);
-// 	map.mapTypes.set('map-style', styledMap);
-// 	map.setMapTypeId('map-style');
-// 	marker.setMap(map);
-// }
+	var map = new google.maps.Map(document.getElementById('map-canvas'),
+		mapOptions);
+	map.mapTypes.set('map-style', styledMap);
+	map.setMapTypeId('map-style');
+	marker.setMap(map);
+}
 
-// google.maps.event.addDomListener(window, 'load', googleMapStyles);
+google.maps.event.addDomListener(window, 'load', googleMapStyles);
 
 
 
